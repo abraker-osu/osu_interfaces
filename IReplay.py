4dcd43e45adb91d814338ab47c2a5e60020a2b91
abraker <abraker95@gmail.com>
2021-01-31 17:19:55 -0500

    Add replay data extraction methods

--- IReplay.py
+++ IReplay.py
@@ -8,3 +8,28 @@ class IReplay:
     @abstractmethod
     def get_play_data(self):
         raise NotImplementedError
+
+
+    @abstractmethod
+    def get_time_data(self):
+        raise NotImplementedError
+
+
+    @abstractmethod
+    def get_press_data(self):
+        raise NotImplementedError
+
+    
+    @abstractmethod
+    def get_xpos_data(self):
+        raise NotImplementedError
+
+
+    @abstractmethod
+    def get_ypos_data(self):
+        raise NotImplementedError
+
+
+    @abstractmethod
+    def get_mania_keys(self):
+        raise NotImplementedError
