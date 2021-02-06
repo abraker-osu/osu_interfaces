3309697ad5ae43394b81e204c8a5b6e6d3aa0320
abraker <abraker95@gmail.com>
2021-02-06 15:04:57 -0500

    Define Hitobject type in IHitobject

--- IHitobject.py
+++ IHitobject.py
@@ -5,6 +5,13 @@ class IHitobject:
 
     __metaclass__ = ABCMeta
 
+    CIRCLE  = 1 << 0
+    SLIDER  = 1 << 1
+    NCOMBO  = 1 << 2
+    SPINNER = 1 << 3
+    # ???
+    MANIALONG = 1 << 7
+
     @abstractmethod
     def pos_x(self):
         raise NotImplementedError
