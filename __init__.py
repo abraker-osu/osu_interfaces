33bf92fad2b2289e5c5374cdef6ef72668579622
abraker95 <abraker95@gmail.com>
2024-12-15 14:43:22 -0500

    Transfer of gamemode and mods classes to osu_interfaces

--- __init__.py
+++ __init__.py
@@ -2,8 +2,14 @@ from .src.IBeatmap import IBeatmap
 from .src.IHitobject import IHitobject
 from .src.IReplay import IReplay
 
+from .src.gamemode import Gamemode
+from .src.mod import Mod
+
+
 __all__ = [
     'IBeatmap',
     'IHitobject',
-    'IReplay'
+    'IReplay',
+    'Gamemode',
+    'Mod'
 ]
